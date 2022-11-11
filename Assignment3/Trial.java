421dce1e24a5b74505e8d15abd7348ecf04042f3
mola73 <117201273+mola73@users.noreply.github.com>
2022-11-11 10:49:30 -0800

    Add files via upload

--- Assignment3/Trial.java
+++ Assignment3/Trial.java
@@ -8,19 +8,78 @@ package com.mycompany.unbccpscassignment3;
  *
  * @author muham
  */
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Trial {
     public static void main(String[]args){
-        //int x []=new x [0x1]; //16 in decimal
-        String x= "MuhammadSAW";
-        System.out.println(x.charAt(0));
-        System.out.println(x.charAt(1));
-        System.out.println(x.charAt(2));
-        
+//        int x []=new int [0x11];//16 in decimal
+//        //an octal and hexidecimal array size works
+//        x[0]=1;
+//        x[5]=23;
+//        x[12]=12;
+//        x[15]=15;
+//        for(int k=0;k< x.length;k++){
+//            System.out.println(x[k]);
+//            
+//        }
         //
+        //
+        //
+        /////////////////////////////////////////////////////////////////////////////////////
+        //String practice
+        String x= "MuhammadSAW";
+        for(int i=0 ; i<11; i++){
+            System.out.println(x.charAt(i));
+        }
+       String y;
+       // y = x.getChars(2, 9,  , 1);
+       System.out.println(x.indexOf('a'));
+       System.out.println(x.substring(2,8));
+        System.out.println(x.substring(3));
+        System.out.println(x.split("hsiahian",5));
         
-        x.getChars(0, 5, 0);
         
-        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //tell user to keep putting numbers in arraylist until they input -1
+ 
+//        ArrayList <Integer> a = new ArrayList<>();
+//        Scanner sc = new Scanner(System.in);
+//        int num=1;
+//        while (num > 0){
+//        System.out.println("Please input an integer");
+//        num= sc.nextInt();
+//        a.add(num); // add a number into an array list named a
+//         System.out.println(a);
+//        } 
+//        System.out.println(a);
+//        System.out.println(arraverage(a));
+//   //complete
+//  System.out.println( a.indexOf(5));
+//    System.out.println( a.lastIndexOf(5));
+//    a.remove(5); System.out.println(a);
+//     System.out.println( a);
+//     System.out.println( a.size());
+//        
+   
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      
+ ////////////////////////////////////////////////////////////////////////////////////////////////////     
+        //  a code that takes in names of studnets and grades, then calculate the average.
+//        public static int gradeave(ArrayList<Integer>student, ArrayList<Integer>Grade){
+//            
+//        }
+//// take the average of all the numbers input
+//    public static int arraverage(ArrayList<Integer> nums){
+//        int sum=0;
+//        int average;
+//        for (Integer a1 : nums) {
+//            sum +=nums.get(a1);
+//        }
+//        average= sum/nums.size()-1;
+//        return average;
+        
+//        }
+    
     
 }
