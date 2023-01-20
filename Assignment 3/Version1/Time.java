1a7aa2bf5f0527010dc233bf6ddb3ce6d239890f
mola73 <117201273+mola73@users.noreply.github.com>
2023-01-20 15:19:06 -0800

    Add files via upload

---
+++ Assignment 3/Version1/Time.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Version1;
+
+/**
+ *
+ * @author molaniyan
+ */
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    public Time() {
+        //creates midnight
+            }
+public Time(int h, int s, int m){
+   this .hours=h;
+   this.minutes
+}
+Time(Time t){
+    
+    (initialize from another Time object
+}
