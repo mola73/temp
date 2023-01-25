a7c3ab53c6fa931c150226cde70de54a2826a4ee
mola73 <117201273+mola73@users.noreply.github.com>
2023-01-25 10:17:12 -0800

    Add files via upload

---
+++ CS101/Assignment 3/Version2c/Timeinterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Version2c;
+
+/**
+ *
+ * @author muham
+ */
+public class Timeinterface {
+    public interface TimeInterface{
+int gerhours();
+int getminutes() ;
+int getseconds() ;
+}
+}
